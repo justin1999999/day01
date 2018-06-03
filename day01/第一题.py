ca986f1fe3ef75acb5886fb25ac344cfebd65970
justin1999999 <39868169+justin1999999@users.noreply.github.com>
2018-06-03 09:54:51 +0800

    Add files via upload
    这是第一天作业

---
+++ day01/第一题.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 09:50:06 2018
+
+@author: lenovo
+"""
+
+
+a=['星期一','星期二','星期三','星期四','星期五','星期六','星期天']
+if a[5]=='星期六':
+   
+   print('今天是'+a[5])
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[6])
